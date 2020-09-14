d18ddfcdff3e836c614636cbc8986e2a85d20dc5
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-09-14 19:36:55 +0800

    [Exodia Website - 3D System] - Added database connection and real time clock

--- Index.php
+++ Index.php
@@ -1,22 +1,43 @@
+<?php
+include_once 'include/DBconnect.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/Time.js"> </script>
     <title>Exodia_Worker_landingpage
     </title>
 </head>
-<body>
+<body onload="realtimeClock()">
    
     <img class="Exodia_Logo" src="images/Exodia_Logo.png" alt=>
 
-    <div class="Time">3:58
+    <div class="Time" id = "Time">
     </div>
 
     <div class="Black_Background">
             <div class="EXODIA">EXODIA
             </div>  
-            <div class="QUOTES">"A true champion can adapt to anything"
+            <?php
+                $sql = "SELECT * FROM banner;";
+                $result = mysqli_query($conn,$sql);
+                $resultCheck = mysqli_num_rows($result);
+                $QoutesData = array();
+
+                if ($resultCheck > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        array_push($QoutesData,$row['banner_name']);
+                    }
+
+                    echo '<div class="QUOTES">';
+                    echo $QoutesData[rand(0,count($QoutesData) - 1)];
+                    echo '</div> ';
+                }
+            ?>
+            
             </div>  
             <a href="#" class="button">INITIALIZE</a>
     </div>
---
+++ include/DBconnect.php
@@ -0,0 +1,8 @@
+<?php
+
+$DBServerName = "localhost:3306";
+$DBUsername = "exodia_nivelle";
+$DBPassword = "NivelleSkyGod1@";
+$DBName = "exodia_Demo";
+
+$conn = mysqli_connect($DBServerName,$DBUsername,$DBPassword,$DBName);
