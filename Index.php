7e93c0abb7e71a4ebd37b3d9ea79da9c2eb56873
Neil <neilallenbahan07@gmail.com>
2020-09-14 16:32:00 +0800

    [Exodia Website - 3D System] - Adding landing page

---
+++ Index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exodia_Worker_landingpage
+    </title>
+</head>
+<body>
+   
+    <img class="Exodia_Logo" src="images/Exodia_Logo.png" alt=>
+
+    <div class="Time">3:58
+    </div>
+
+    <div class="Black_Background">
+            <div class="EXODIA">EXODIA
+            </div>  
+            <div class="QUOTES">"A true champion can adapt to anything"
+            </div>  
+            <a href="#" class="button">INITIALIZE</a>
+    </div>
+   
+
+   
+</body>
+</html>
+
+<style>
+.Exodia_Logo
+    {
+    position :absolute;
+    left:3%;
+    top:3%;
+    }
+.Time{
+    Font-family:HelveticaNeue;
+    font-weight:300;
+    font-size:72px;
+    position :absolute;
+    right:3%;
+    top:3%;
+
+}    
+.Black_Background
+    {
+    position: absolute;
+    top:23%;
+    left:0%;
+    background-color:black;
+    width:100%;    
+    height:55%;
+    
+    }    
+.EXODIA{
+    Font-family:HelveticaNeue;
+    font-weight:300;
+    letter-spacing:12px; 
+    padding-top:3%;
+    text-align:center;
+    Font-size:72px;
+    Color:white;
+}   
+.QUOTES{
+    Font-family:HelveticaNeue;
+    font-weight:500; 
+    padding-top:3%;
+    text-align:center;
+    Font-size:36px;
+    Color:white;
+}
+.button {
+    Text-align:center;
+    position:absolute;
+    left:43%;
+    top:65%;
+    Font-family:HelveticaNeue;
+    font-weight:600;
+    font-size:25px;  
+    border-radius: 40px;
+    background-color: white;
+    border: none;
+    color: black;
+    padding: 15px ;
+    padding-left:40px;
+    padding-right:40px;
+    text-decoration: none;
+    
+    cursor: pointer;
+}
+
+
+
+</style>
