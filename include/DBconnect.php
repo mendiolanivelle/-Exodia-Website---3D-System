7ff3de624fb26a9d7be7d44ee624f1e50aa0dc6a
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-09-15 16:02:42 +0800

    [Exodia Website - 3D System] - Deleted Index.php

--- include/DBconnect.php
+++ include/DBconnect.php
@@ -3,6 +3,6 @@
 $DBServerName = "localhost:3306";
 $DBUsername = "exodia_nivelle";
 $DBPassword = "NivelleSkyGod1@";
-$DBName = "exodia_Demo";
+$DBName = "exodia_system";
 
 $conn = mysqli_connect($DBServerName,$DBUsername,$DBPassword,$DBName);
